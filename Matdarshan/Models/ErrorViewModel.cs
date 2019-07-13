09af08dcbe90afcde80bd69fbc0954fcecb60f78
prem12 <murmu.1993@gmail.com>
2019-07-13 23:07:40 +0530

    Add project files.

---
+++ Matdarshan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Matdarshan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
